c189ae61710d26a8cc9313723f028d5189bd370c
marcosal <marcos90.alonso90@gmail.com>
2023-01-12 17:57:04 -0300

    arreglo hover

--- codigo.js
+++ codigo.js
@@ -1,29 +1,29 @@
-document.getElementById("imagenef").addEventListener("click",moverse);
+// document.getElementById("imagenef").addEventListener("click",moverse);
 
-function moverse(){
-   if (parrafo1.style.display === "none"){
-    parrafo1.style.display ="block"
-   } else{
-    parrafo1.style.display = "none"
-   }
-}
+// function moverse(){
+//    if (parrafo1.style.display === "none"){
+//     parrafo1.style.display ="block"
+//    } else{
+//     parrafo1.style.display = "none"
+//    }
+// }
 
 
-var icono = document.getElementsByClassName("material-icons");
-var texto = document.getElementsByClassName("parrafo-oreo");
+// var icono = document.getElementsByClassName("material-icons");
+// var texto = document.getElementsByClassName("parrafo-oreo");
 
-var moverse2 = function(i){
-    if (texto[i].style.display === "none"){
-        texto[i].style.display ="block"
-    } else{
-        texto[i].style.display ="none"
-    }   
+// var moverse2 = function(i){
+//     if (texto[i].style.display === "none"){
+//         texto[i].style.display ="block"
+//     } else{
+//         texto[i].style.display ="none"
+//     }   
 
-}
+// }
 
-for (var i=0; i < icono.length; i++){
-    icono[i].addEventListener("click",moverse2.bind(this,i));
-}
+// for (var i=0; i < icono.length; i++){
+//     icono[i].addEventListener("click",moverse2.bind(this,i));
+// }
 
 
 
