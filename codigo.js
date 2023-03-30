3a724629902275a04d6defbd1976e307dfbf02e9
marcosal <marcos90.alonso90@gmail.com>
2023-03-30 19:50:31 -0300

    mejorando proyecto

--- codigo.js
+++ codigo.js
@@ -1,30 +1,15 @@
-// document.getElementById("imagenef").addEventListener("click",moverse);
+document.getElementById("contenedor1").onclick = function () {
+    console.log("Estoy viendo la experiencia laboral del sujeto")
+}
 
-// function moverse(){
-//    if (parrafo1.style.display === "none"){
-//     parrafo1.style.display ="block"
-//    } else{
-//     parrafo1.style.display = "none"
-//    }
-// }
+document.getElementById("contenedor2").onclick = function () {
+    console.log("Estoy viendo las habilidades del sujeto")
+}
 
+document.getElementById("contenedor3").onclick = function () {
+    console.log("Estoy viendo la formación académica del sujeto")
+}
 
-// var icono = document.getElementsByClassName("material-icons");
-// var texto = document.getElementsByClassName("parrafo-oreo");
-
-// var moverse2 = function(i){
-//     if (texto[i].style.display === "none"){
-//         texto[i].style.display ="block"
-//     } else{
-//         texto[i].style.display ="none"
-//     }   
-
-// }
-
-// for (var i=0; i < icono.length; i++){
-//     icono[i].addEventListener("click",moverse2.bind(this,i));
-// }
-
-
-
-
+function info (){
+    document.getElementById("cuerpo").style.color ="white";
+}   
